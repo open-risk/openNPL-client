e32c0aae1ef2ea75b6dc32cd1ff71c70b253560d
Open Risk <openrisk@outlook.com>
2026-05-20 17:10:26 +0200

    move to uv; expand readme

--- curl/sflp_data.sh
+++ curl/sflp_data.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
+
 source ../.env
 curl http://127.0.0.1:${PORT}/api/sflp_data/
---
+++ main.py
@@ -0,0 +1,6 @@
+def main():
+    print("Hello from openNPL-client!")
+
+
+if __name__ == "__main__":
+    main()
